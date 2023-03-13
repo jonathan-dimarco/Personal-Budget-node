02e11f20bd67a021fc43c629bdc42b4668ddea6e
Jonathan_DiMarco <jonathandimarcoq@gmail.com>
2023-03-13 23:05:00 +0100

    added envelopes database

---
+++ db.js
@@ -0,0 +1,41 @@
+let totalBudget = 0;
+
+let envelopes = [
+{   id: 1,
+    name: "rent",
+    budget: 0,
+},
+{   id: 2,
+    name: 'groceries',
+    budget: 0,
+},
+{
+    id: 3,
+    name: 'gas',
+    budget: 0,
+},
+{
+    id: 4,
+    name: 'services',
+    budget: 0,
+},
+{
+    id: 5,
+    name: 'healthcare',
+    budget: 0,
+},
+{
+    id:6,
+    name: 'others',
+    budget: 0,
+},
+{
+    id: 7,
+    name: "savings",
+    budget: 0,
+},
+];
+
+//helper functions
+
+
