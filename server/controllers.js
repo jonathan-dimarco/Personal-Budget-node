466de151903809b859e51bdaafe495169151c464
Jonathan_DiMarco <jonathandimarcoq@gmail.com>
2023-03-18 11:46:38 +0100

    added createId function

--- server/controllers.js
+++ server/controllers.js
@@ -1,14 +1,23 @@
 const envelopes = require('./db');
 
+const createId = (envelopes) => {
+  const id = envelopes.length + 1;
+  return id;
+}
+
 const getAllEnvelopes =  () => {
 return envelopes;
 }
 
-const addEnvelope =  (instance) => {
-  const newEnvelope =  instance;
+const addEnvelope = (instance) => {
+  const { name, budget } = instance;
+  const newId = createId(envelopes);
+  const newEnvelope = {
+    id: newId,
+    name: name,
+    budget: budget
+  };
   envelopes.push(newEnvelope);
 }
-
-
 
 module.exports = { getAllEnvelopes, addEnvelope};
